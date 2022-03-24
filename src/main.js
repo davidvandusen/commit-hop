e5a0b015958a3961b2dd9c66ce7a6e6a6702f707
David VanDusen <github@dvw3.com>
2022-03-24 15:07:03 -0700

    Use TLS and default port for connection to websocket server

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ indexeddbProvider.whenSynced.then(() => {
 });
 
 const websocketProvider = new WebsocketProvider(
-  'ws://ywss.figureandsound.com:1234',
+  'wss://ywss.figureandsound.com',
   'count-demo',
   ydoc
 );
