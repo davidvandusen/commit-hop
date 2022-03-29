f4169b5defae0fec0d5c0d57b73c9da1af07b910
David VanDusen <github@dvw3.com>
2022-03-29 12:43:15 -0700

    Add ability to delete nodes and nest them under other nodes

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import {
   Composite,
   Constraint,
   Engine,
+  Events,
   Mouse,
   MouseConstraint,
+  Query,
   Render,
   Runner,
 } from 'matter-js';
@@ -25,6 +27,7 @@ const makeTreeComposite = tree => {
       collisionFilter: { group: -1 },
       render: { sprite: { texture: node.texture } },
     });
+    shape.yid = node.id;
     bodies.push(shape);
     if (mode === modes.EDIT) {
       constraints.push(
@@ -48,10 +51,13 @@ const makeTreeComposite = tree => {
           bodyB: childShape,
           pointA: { x: 0, y: 0 },
           pointB: { x: 0, y: 0 },
-          stiffness: stiffness,
-          render: {
-            visible: false,
-          },
+          stiffness: 1,
+          render:
+            mode === modes.PLAY
+              ? { visible: false }
+              : {
+                  strokeStyle: 'rgba(255,255,255,0.25)',
+                },
         })
       );
       childShapes.push(childShape);
@@ -97,6 +103,16 @@ const createTheWorld = element => {
   });
   Composite.add(engine.world, mouseConstraint);
   render.mouse = mouse;
+  const trash = Bodies.rectangle(20, 570, 50, 50, {
+    isStatic: true,
+    render: {
+      sprite: {
+        texture: '/trash.png',
+        xScale: 0.5,
+        yScale: 0.5,
+      },
+    },
+  });
   Composite.add(engine.world, [
     Bodies.rectangle(400, 0, 800, 50, {
       isStatic: true,
@@ -114,11 +130,12 @@ const createTheWorld = element => {
       isStatic: true,
       render: { visible: false },
     }),
+    trash,
   ]);
   Render.run(render);
   const runner = Runner.create();
   Runner.run(runner, engine);
-  return engine.world;
+  return { mouseConstraint, trash, world: engine.world };
 };
 
 const animateLife = (data, world) => {
@@ -132,9 +149,10 @@ const main = () => {
   new WebsocketProvider('wss://ywss.figureandsound.com', 'data', ydoc);
   let life;
   const app = document.getElementById('app');
-  const world = createTheWorld(app);
+  const { mouseConstraint, trash, world } = createTheWorld(app);
   const addLife = (x, y, r, image) => {
     const newChild = new Y.Map();
+    newChild.set('id', `${ydoc.clientID}-${Date.now()}`);
     newChild.set('x', x);
     newChild.set('y', y);
     newChild.set('r', r);
@@ -145,6 +163,7 @@ const main = () => {
   };
   const removeLife = () => {
     const root = ydoc.getMap('root');
+    root.set('id', 'root');
     root.set('x', 400);
     root.set('y', 300);
     root.set('r', 50);
@@ -180,7 +199,7 @@ const main = () => {
       event.dataTransfer.setData(
         'application/json',
         JSON.stringify({
-          image: (new URL(event.target.src)).pathname,
+          image: new URL(event.target.src).pathname,
           size: event.target.width / 2,
         })
       );
@@ -191,6 +210,7 @@ const main = () => {
     event.preventDefault();
   });
   canvas.addEventListener('drop', event => {
+    if (mode !== modes.EDIT) return;
     const raw = event.dataTransfer.getData('application/json');
     let parsed;
     try {
@@ -200,6 +220,86 @@ const main = () => {
       return;
     }
     addLife(event.offsetX, event.offsetY, parsed.size, parsed.image);
+  });
+  // Clicking on two objects will nest them. Trying to nest an object to the
+  // trash can will delete it and its children.
+  let nestBodyA;
+  let nestBodyB;
+  const destroyObject = yid => {
+    const stack = [ydoc.getMap('root')];
+    while (stack.length) {
+      const node = stack.pop();
+      if (node.get('id') === yid) {
+        const yArray = node.parent;
+        if (yArray) {
+          yArray.delete(yArray.toArray().indexOf(node), 1);
+        }
+        return;
+      }
+      stack.push(...node.get('children'));
+    }
+  };
+  const nestObjects = (yidA, yidB) => {
+    let objA;
+    let objB;
+    const stack = [ydoc.getMap('root')];
+    while (stack.length) {
+      const node = stack.pop();
+      if (node.get('id') === yidA) {
+        objA = node;
+      }
+      if (node.get('id') === yidB) {
+        objB = node;
+      }
+      if (objA && objB) {
+        // TODO if B is a descendant of A then cancel
+        const cloneA = objA.clone();
+        const parentA = objA.parent;
+        if (parentA) {
+          parentA.delete(parentA.toArray().indexOf(objA), 1);
+        }
+        objB.get('children').insert(0, [cloneA]);
+        return;
+      }
+      stack.push(...node.get('children'));
+    }
+  };
+  Events.on(mouseConstraint, 'mousedown', event => {
+    if (mode !== modes.EDIT) return;
+    const [hoveredBody] = Query.point(
+      [...Composite.allBodies(life), trash],
+      event.mouse.position
+    );
+    if (hoveredBody) {
+      if (!nestBodyA) {
+        nestBodyA = hoveredBody;
+      } else if (hoveredBody !== nestBodyA) {
+        nestBodyB = hoveredBody;
+      }
+      if (nestBodyA && nestBodyB) {
+        let destroyBody;
+        if (nestBodyA === trash) {
+          destroyBody = nestBodyB;
+        }
+        if (nestBodyB === trash) {
+          destroyBody = nestBodyA;
+        }
+        if (destroyBody) {
+          if (destroyBody.yid === 'root') {
+            alert("Can't delete that one. Sorry!");
+          } else if (destroyBody.yid) {
+            destroyObject(destroyBody.yid);
+          }
+        }
+        if (nestBodyA.yid === 'root') {
+          alert("Can't move that one. Sorry!");
+        } else if (nestBodyA.yid && nestBodyB.yid) {
+          nestObjects(nestBodyA.yid, nestBodyB.yid);
+        }
+        nestBodyA = null;
+        nestBodyB = null;
+      }
+    }
   });
 };
 
